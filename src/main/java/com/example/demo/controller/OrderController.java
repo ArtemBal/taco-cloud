92af14be88e52b43549fb6cc486576602085bec3
Artem Balan <a.s.balan96@gmail.com>
2023-11-19 20:43:11 +0200

    Add rules of validation

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 import com.example.demo.tacos.TacoOrder;
+
+import jakarta.validation.Valid;
 
 @Slf4j
 @Controller
@@ -22,7 +25,10 @@ public class OrderController {
 	}
 	
 	@PostMapping
-	public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+	public String processOrder(@Valid TacoOrder order, Errors error, SessionStatus sessionStatus) {
+		if (error.hasErrors()) {
+			return "orderForm";
+		}
 		log.info("Order submitted: {}", order);
 		sessionStatus.setComplete();
 		return "redirect:/";
