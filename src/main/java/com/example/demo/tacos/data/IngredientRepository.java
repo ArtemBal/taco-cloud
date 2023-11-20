18d236d734f4223532b4e06d165a571ae9cd2989
Artem Balan <a.s.balan96@gmail.com>
2023-11-20 22:51:22 +0200

    Write JdbcIngredientRepository to work with db

---
+++ src/main/java/com/example/demo/tacos/data/IngredientRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.tacos.data;
+
+import java.util.Optional;
+
+import com.example.demo.tacos.Ingredient;
+
+public interface IngredientRepository {
+	Iterable<Ingredient> findAll();
+    Optional<Ingredient> findById(String id);
+    Ingredient save(Ingredient ingredient);
+}
