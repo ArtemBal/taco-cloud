d881824b2149a74443f44354988caad0ca9c6e39
Artem Balan <a.s.balan96@gmail.com>
2023-11-20 23:02:20 +0200

    Change methods to work with JdbcTemplate

--- src/main/java/com/example/demo/controller/DesignTacoController.java
+++ src/main/java/com/example/demo/controller/DesignTacoController.java
@@ -1,9 +1,10 @@
 package com.example.demo.controller;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,6 +18,7 @@ import com.example.demo.tacos.Ingredient;
 import com.example.demo.tacos.Ingredient.Type;
 import com.example.demo.tacos.Taco;
 import com.example.demo.tacos.TacoOrder;
+import com.example.demo.tacos.data.IngredientRepository;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -27,14 +29,18 @@ import lombok.extern.slf4j.Slf4j;
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
+	private final IngredientRepository ingredientRepo;
+
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
+	}
+
 	@ModelAttribute
 	public void addIngredientsToModel(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-				new Ingredient("COTO", "Corn Tortilla", Type.WRAP), new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES), new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-				new Ingredient("CHED", "Cheddar", Type.CHEESE), new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-				new Ingredient("SLSA", "Salsa", Type.SAUCE), new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+		Iterable<Ingredient> ingredientsFromJdbc = ingredientRepo.findAll();
+		List<Ingredient> ingredients = new ArrayList<>();
+		ingredientsFromJdbc.forEach(ingredients::add);
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
